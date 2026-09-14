f14cb9afff47911949ad64d9c7aa9b4d51b187e7
Adama Traore <adama@example.com>
2026-09-14 09:13:39 +0000

    Ajustement des frais de scolarite nets Raya 2 conformes a la deduction des frais annexes

---
+++ supabase/migrations/023_adjust_raya2_tuition_fees.sql
@@ -0,0 +1,19 @@
+-- Ajustement des frais de scolarite nets pour Raya Islam 2 (Forfait global - Frais Annexes de 9 000 F)
+-- 33 000 - 9 000 = 24 000 (Maternelle et Primaire 1-3)
+-- 34 000 - 9 000 = 25 000 (Primaire 4-5)
+-- etc.
+UPDATE classes SET tuition_fee = 24000, tuition_fee_affecte = 24000 WHERE id = 'a1b698be-5c23-4d63-b16e-dd0bd32e5227';
+UPDATE classes SET tuition_fee = 24000, tuition_fee_affecte = 24000 WHERE id = '14c2a3f6-6c87-49af-8382-85846433a7c9';
+UPDATE classes SET tuition_fee = 24000, tuition_fee_affecte = 24000 WHERE id = 'b9dcc4c9-dfa1-44b2-91e1-73acd3d84871';
+UPDATE classes SET tuition_fee = 24000, tuition_fee_affecte = 24000 WHERE id = '850ac641-97b1-4c54-94f0-b7a8e1e3f24d';
+UPDATE classes SET tuition_fee = 25000, tuition_fee_affecte = 25000 WHERE id = '452e684f-5edf-4740-b3fd-e0a48e08f349';
+UPDATE classes SET tuition_fee = 25000, tuition_fee_affecte = 25000 WHERE id = '87fe8743-0355-409f-8f5a-0852b5cddcbd';
+UPDATE classes SET tuition_fee = 31000, tuition_fee_affecte = 31000 WHERE id = '1c8dfd82-b1e4-4f60-8bff-830fc7d0db07';
+UPDATE classes SET tuition_fee = 33500, tuition_fee_affecte = 33500 WHERE id = 'ad0b6980-21db-4c66-81d9-cc214e101ec8';
+UPDATE classes SET tuition_fee = 33500, tuition_fee_affecte = 33500 WHERE id = 'c96f3f5e-c9f9-4e1d-893b-5e4ec3d97afd';
+UPDATE classes SET tuition_fee = 33500, tuition_fee_affecte = 33500 WHERE id = 'd218d2d4-9de3-4721-8e1d-87e2cbcfad98';
+UPDATE classes SET tuition_fee = 33500, tuition_fee_affecte = 33500 WHERE id = 'ff426812-5606-4c0a-924a-9cf11e987b20';
+UPDATE classes SET tuition_fee = 39500, tuition_fee_affecte = 39500 WHERE id = 'e67cf242-2fdd-46a3-861a-30691ef5fec8';
+UPDATE classes SET tuition_fee = 36000, tuition_fee_affecte = 36000 WHERE id = 'b57c83c8-43ec-412f-a520-cb3cf8822cca';
+UPDATE classes SET tuition_fee = 36000, tuition_fee_affecte = 36000 WHERE id = '9b01fdfe-c184-4df1-b0c0-e2713a72a1de';
+UPDATE classes SET tuition_fee = 44000, tuition_fee_affecte = 44000 WHERE id = '489010bf-57e9-42b0-9d26-90bfefecf3f6';
